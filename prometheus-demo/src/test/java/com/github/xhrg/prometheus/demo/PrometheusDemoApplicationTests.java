95252ba3a75be4a48bc7c79fa552a7ed9dfac541
xhrg <634789257@qq.com>
2022-08-25 14:42:40 +0800

    prometheus

---
+++ prometheus-demo/src/test/java/com/github/xhrg/prometheus/demo/PrometheusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.prometheus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
